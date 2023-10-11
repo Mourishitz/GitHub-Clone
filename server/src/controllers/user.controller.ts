e0e588c81b3ce0ec21e0db26546b16facc0d3006
Mourishitz <gabrielmorishita@hotmail.com>
2023-10-11 00:14:48 -0300

    👽️ feat: integrating and maping GitHub API

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,12 +1,51 @@
 import express, { type Request, type Response, type Router } from 'express'
+import GitHubService from '../services/github.service'
+import UserResource from '../resources/user.resource'
+import RepositoryResource from '../resources/repository.resource'
 
 const userController: Router = express.Router()
 
-userController.get('/', (request: Request, response: Response) => {
-  // TODO: Implement UserService calls to GitHub
+userController.get('/', async (request: Request, response: Response) => {
+  const [since, limit] = [request.query['since'], request.query['limit']];
+
+  const users: UserResource[] = await GitHubService.getUsers(+since, +limit);
 
   response.json({
-    'message': 'hi mom!'
+    'data': users
+  })
+})
+
+userController.get('/:username/details', async(request: Request, response: Response) => {
+  const user: UserResource = await GitHubService.getUser(request.params['username']);
+
+  if(!user){
+    response.status(404);
+    response.json({
+      'message': 'User not found.'
+    });
+    return;
+  }
+
+  response.json({
+    'data': user
+  })
+})
+
+userController.get('/:username/repos', async(request: Request, response: Response) => {
+  const user: UserResource = await GitHubService.getUser(request.params['username']);
+
+  if(!user){
+    response.status(404);
+    response.json({
+      'message': 'User not found.'
+    });
+    return;
+  }
+
+  const repositories: RepositoryResource[] = await GitHubService.getRepositoriesForUser(user.login);
+
+  response.json({
+    'data': repositories
   })
 })
 
--- server/src/services/github.service.ts
+++ server/src/services/github.service.ts
@@ -1,7 +1,72 @@
+import RepositoryResource from '../resources/repository.resource';
 import type UserResource from '../resources/user.resource'
+import { AxiosHttpService } from './http/axios-http.service'
+import { HttpServiceException } from './http/exceptions/HttpServiceException';
 
-class GitHubService {
-  // TODO: Integrate GitHub API endpoints
+class GitHubService extends AxiosHttpService {
+
+  constructor(){
+    super('https://api.github.com');
+  }
+
+  /**
+   * Get an array of Users based on parameters passed.
+   * 
+   * @param since 
+   * @param limit 
+   * @returns Promise<UserResource[]>
+   */
+  public async getUsers(since?: number, limit: number = 10): Promise<UserResource[]>{
+    const response = await this.get(`/users?since=${since}&per_page=${limit}`, {
+      headers: {
+        Accept: 'application/vnd.github+json'
+      }
+    });
+
+    return response.data as UserResource[];
+  }
+
+  /**
+   * Get a User object by username.
+   * 
+   * @param username 
+   * @returns Promise<UserResource>
+   * @throws HttpServiceException
+   */
+  public async getUser(username: string): Promise<UserResource>{
+    try {
+      const response = await this.get(`/users/${username}`, {
+        headers: {
+          Accept: 'application/vnd.github+json'
+        }
+      });
+
+      if(response.status === 404){
+        throw new HttpServiceException('User not found.');
+      } 
+
+      return response.data as UserResource;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  public async getRepositoriesForUser(username: string): Promise<RepositoryResource[]> {
+    try {
+      const user: UserResource = await this.getUser(username);
+
+      if(!user){
+        throw new HttpServiceException('User not found');
+      }
+
+      const repository = await this.get(`/users/${username}/repos`);
+
+      return repository.data as RepositoryResource[]
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
 
 export default new GitHubService()
